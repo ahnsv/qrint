ed1fe123f8e38955f4411198f8dbd59022f65255
ahnsv <ahnsv@bc.edu>
2024-07-18 14:01:13 +0900

    feat: add file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import { Button } from '@/components/ui/button'
+import FileUploadDropzone from '@/components/ui/file-uploader';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello</Button>
+    <div className='w-screen h-screen flex items-center justify-center flex-col'>
+      <form className='w-3/4'>
+        <FileUploadDropzone />
+        <Button className='w-48 my-8'>Upload</Button>
+      </form>
     </div>
   );
 }
